0c7dbec064fbf227ff287a080a1b78dbe0d981fe
Michael_Schrijvers_Thuis <michael.10330@hotmail.com>
2024-06-18 15:01:13 +0200

    snelle push mark is een bitch

---
+++ Assets/Myassets/Scripts/Change_scene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change_scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Scene_Manager.instance.TurnOffUI();
+            Scene_Manager.instance.LoadScene(1);
+        }
+    }
+}
